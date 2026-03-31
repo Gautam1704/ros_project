c4f79d4790ea9e900b3b8a0653519ff2873694a3
Gautam1704 <gautamtotlani174@gmail.com>
2026-03-31 23:32:02 +0530

    hi

--- src/ros_project/launch/actual_robot.launch.py
+++ src/ros_project/launch/actual_robot.launch.py
@@ -13,7 +13,7 @@ def generate_launch_description():
     pkg_ros_gz_rbot = get_package_share_directory('ros_project')
 
 
-    robot_description_file = os.path.join(pkg_ros_gz_rbot, 'urdf', 'slam.xacro')
+    robot_description_file = os.path.join(pkg_ros_gz_rbot, 'urdf', 'slam_lidar_midplate_front.xacro')
     ros_gz_bridge_config = os.path.join(pkg_ros_gz_rbot, 'config', 'ros_gz_bridge_gazebo.yaml')
     
     robot_description_config = xacro.process_file(robot_description_file)
